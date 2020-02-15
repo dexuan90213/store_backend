ec417acf19a36acbdde3ac08061ccf5131ad6838
Eden Yang <dexuan90213@gmail.com>
2020-02-15 15:33:02 +0800

    add product CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,52 @@
 class Admin::ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path, notice: '商品新增成功'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to edit_admin_product_path(@product), notice: '商品資料已更新'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice: '商品已刪除'
+  end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name,
+                                    :desc,
+                                    :state)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_15_065809) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
+    t.string "state"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
