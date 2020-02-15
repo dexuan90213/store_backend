f2aa044e6dade76d1a98fa41e2a97b51f82ef2cb
Eden Yang <dexuan90213@gmail.com>
2020-02-15 17:57:03 +0800

    add backend layout and devise login view

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,6 +2,8 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_manager!
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
+  layout 'backend'
+
   def index
     @products = Product.all
   end
