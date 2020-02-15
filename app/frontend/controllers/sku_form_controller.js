ff25faaef8af9bf22ef5ca83d7614e71ce300068
Eden Yang <dexuan90213@gmail.com>
2020-02-16 02:02:06 +0800

    add sku from remove_item feature

--- app/frontend/controllers/sku_form_controller.js
+++ app/frontend/controllers/sku_form_controller.js
@@ -9,4 +9,12 @@ export default class extends Controller {
     let content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
     this.add_itemTarget.insertAdjacentHTML('beforebegin', content)
   }
+
+  remove_sku(evt) {
+    evt.preventDefault()
+
+    let item = event.target.closest('.nested-fields')
+    item.querySelector("input[name*='_destroy']").value = 1
+    item.style.display = 'none'
+  }
 }
